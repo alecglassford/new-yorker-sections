2c1cf03652cfabbd9a2d787515c340672f266e8b
alecglassford <glassford@stanford.edu>
2017-02-12 14:23:57 -0500

    better..

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ var viz = d3.select('#viz');
 
 function dotGenerator(max) {
     return function(val) {
-        return '.'.repeat(val/max * 100);
+        return '.'.repeat(val/max * 80);
     };
 }
 
 function sortByLength (a, b) {
     return d3.descending(a.values.length, b.values.length);
+}
+
+function sortByDate (a, b) {
+    return d3.descending(a.date, b.date);
 }
 
 d3.csv('new-yorker-sections.csv', function(rows) {
@@ -21,17 +25,33 @@ d3.csv('new-yorker-sections.csv', function(rows) {
             .entries(rows)
             .sort(sortByLength);
         updateSections(sections);
+        var authors = d3.nest()
+            .key(d => d.author_name)
+            .entries(rows)
+            .sort(sortByLength);
+        updateAuthors(authors);
+        viz.select('#articles').html('');
     };
     d3.select('#reset').on('click', reset);
     reset();
 });
 
-function selectSection(section_data) {
+function selectAuthor(section_data) {
+    d3.select('#authors')
+        .selectAll('div')
+        .classed('selected', false);
+    d3.select(this)
+        .classed('selected', true);
+
     var sections = d3.nest()
         .key(d => d.section_name)
         .entries(section_data.values)
         .sort(sortByLength);
     updateSections(sections);
+    var articles = sections.map(d => d.values)
+        .reduce((a,b)=>a.concat(b))
+        .sort(sortByDate);
+    updateArticles(articles);
 }
 
 function updateSections(sections) {
@@ -43,16 +63,26 @@ function updateSections(sections) {
         .enter()
             .append('div')
             .attr('class', 'item')
-            .html(d => (d.key.length ? d.key : 'No author listed') + ' ' +  '<br>' + sectionDots(d.values.length) + ' ' + String(d.values.length))
-            .on('click', selectAuthor);
+            .html(d => (d.key.length ? d.key : 'No section listed') + ' ' +  '<br>' + sectionDots(d.values.length) + ' ' + String(d.values.length))
+            .on('click', selectSection);
 }
 
-function selectAuthor(author_data) {
+function selectSection(author_data) {
+    d3.select('#sections')
+        .selectAll('div')
+        .classed('selected', false);
+    d3.select(this)
+        .classed('selected', true);
+
     var authors = d3.nest()
         .key(d => d.author_name)
         .entries(author_data.values)
         .sort(sortByLength);
     updateAuthors(authors);
+    var articles = authors.map(d => d.values)
+        .reduce((a,b)=>a.concat(b))
+        .sort(sortByDate);
+    updateArticles(articles);
 }
 
 function updateAuthors(authors) {
@@ -65,5 +95,22 @@ function updateAuthors(authors) {
             .append('div')
             .attr('class', 'item')
             .html(d => (d.key.length ? d.key : 'No author listed') + ' ' +  '<br>' + authorDots(d.values.length) + ' ' + String(d.values.length))
-            .on('click', selectSection);
+            .on('click', selectAuthor);
+}
+
+function updateArticles(articles) {
+    viz.select('#articles').html('');
+    viz.select('#articles')
+        .selectAll('div')
+        .data(articles)
+        .enter()
+            .append('div')
+            .attr('class', 'item')
+            .html(function(d) {
+                return `<a href="${d.article_url}"><strong>${d.article_name}</strong></a><br>
+                        by <a href="${d.author_url}">${d.author_name}</a><br>
+                        in <a href="${d.section_url}">${d.section_name}</a><br>
+                        on ${d.date}
+                        <p>${d.summary_text}</p>`;
+            });
 }
